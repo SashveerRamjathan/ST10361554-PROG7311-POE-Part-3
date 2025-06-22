2830e916d147456932652409dbb68625d2e7fee7
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2025-06-22 23:27:50 +0200

    Add About page and controller action
    Implemented a new `About` action method in `HomeController.cs` to return the About view. Updated `About.cshtml` with detailed content about the Agri-Energy Connect platform, including its purpose, key features, operational workflow, technical architecture, and target audience.

--- Agri-Energy Connect/Controllers/HomeController.cs
+++ Agri-Energy Connect/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace Agri_Energy_Connect.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
